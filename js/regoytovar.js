31c76ca7a69ad51751b43a38c14ba0769653c981
regoytovar <regoytovar@gmail.com>
2017-10-30 20:46:00 -0400

    Drosolca, tu aliada en salud

---
+++ js/regoytovar.js
@@ -0,0 +1,12 @@
+var typed = new Typed('#type-header',{
+    strings:[
+        "medicamentos.",
+        "equipos médicos.",
+        "material médico quirúrgico.",
+        "misceláneos",
+        "insumos hospitalarios."
+    ],
+    typeSpeed: 75,
+    loop: true,
+    backDelay: 1000
+})
